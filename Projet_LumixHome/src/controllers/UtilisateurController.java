f3050636f47837b7443f23684c2ace73d44e88ac
RayaneC-01 <rchaabane20@gmail.com>
2025-02-05 11:12:39 +0100

    Ajout des controllers pour gérer les interactions  et la lecture métier:  Lumiere, Utilisateur,Piece, Equipement,Capteur

---
+++ Projet_LumixHome/src/controllers/UtilisateurController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.Utilisateur;
+
+public class UtilisateurController {
+    private Utilisateur utilisateur;
+
+    public UtilisateurController(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public void inscrireUtilisateur(String nom, String prenom, String email, String motDePasse) {
+        utilisateur.inscription_utilisateur(nom, prenom, email, motDePasse);
+    }
+
+    public boolean authentifierUtilisateur(String email, String motDePasse) {
+        return utilisateur.authentification_utilisateur(email, motDePasse);
+    }
+
+    public void deconnecterUtilisateur() {
+        utilisateur.deconnexion();
+    }
+
+    public void reinitialiserMotDePasse(String email) {
+        utilisateur.reinitialiserMotDePasse(email);
+    }
+}
